4932e6aa3753aca557f907d6dbdeaed187593c7b
Alex Silva da Rocha <alexsilvadarocha@gmail.com>
2022-04-14 21:38:18 -0300

    Aula- Operadores e lógica de programação

---
+++ modulo1/aula - Operadores e Lógica de Programação/index.js
@@ -0,0 +1,33 @@
+//1. 
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+//false
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+//false
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+//true
+console.log("d. ", typeof resultado)
+//boleano
+
+
+//2. O problema é que não vai somar duas strings, vai no
+// máximo concatenar os numeros.
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+//3. Solução:
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
---
+++ modulo1/projeto-readme/aprendizados.md
@@ -0,0 +1,2 @@
+# Aprendizados da semana
+Durante a **segunda semana** aprendi sobre comandos em terminal, git, github e incio de vs code.
