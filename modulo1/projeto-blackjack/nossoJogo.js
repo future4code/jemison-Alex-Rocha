9b18a8bf534f930d41624af67abcf294ed6d68c5
Alex Silva da Rocha <alexsilvadarocha@gmail.com>
2022-05-08 23:58:43 -0300

    Fiz o máximo que eu pensei na solucao

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,9 +10,97 @@
  * 
  * 
  */
-console.log("Boas vindas ao jogo de Blackjack!")
-if(confirm("Quer iniciar uma nova rodada?")){
+   //  console.log("Boas vindas ao jogo de Blackjack!")
+    if(confirm("Quer iniciar uma nova rodada?")){
+    
+    
+    
+    //console.log("Boas vindas ao jogo de Blackjack!")
+    const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+    //const naipes = [...cartas,...cartas,...cartas,...cartas]
+    const naipes = ["♦️", "♥️", "♣️", "♠️"]
+    
+    let pontosUsuario = 0
+    let pontosMaquina = 0
+    
+     function cartasUsuario(){
+    //randomizacao de cartas
+    
+    const numero = cartas[Math.floor(Math.random() * 13)]
+    const naipe = naipes[Math.floor(Math.random() * 4)] 
+    
+    let valor
+    if (numero === "A") {
+      valor = 11
+    } else if (numero === "J" || numero === "Q" || numero === "K") {
+      valor = 10
+    } else { 
+      valor = Number(numero)
+    }
+    
+    const carta = {
+      texto: numero + naipe,
+      valor: valor
+    }
+    
+    return carta
+    
+    
+    pontosUsuario += carta.valor
+    pontosUsuario += carta.valor
+    }
+    console.log(cartasUsuario.carta.texto)
+    console.log(cartasUsuario.carta.valor)
 
-} else{
-   console.log("O jogo acabou.")
-}
+    function cartasMaquina(){
+      //randomizacao de cartas
+      
+      const numero = cartas[Math.floor(Math.random() * 13)]
+      const naipe = naipes[Math.floor(Math.random() * 4)] 
+      
+      let valor
+      if (numero === "A") {
+        valor = 11
+      } else if (numero === "J" || numero === "Q" || numero === "K") {
+        valor = 10
+      } else { 
+        valor = Number(numero)
+      }
+      
+      const carta = {
+        texto: numero + naipe,
+        valor: valor
+      }
+      
+      return carta
+      
+    pontosMaquina += carta.valor
+    pontosMaquina += carta.valor
+    }
+    console.log(cartasMaquina.carta.texto)
+    console.log(cartasMaquina.carta.valor)
+    
+    function verificarCartas (){
+      if(pontosUsuario === 21){
+        console.log("Usuario Ganhou!")
+      } else if (pontosMaquina === 21){
+        console.log("Você perdeu!")
+      }else if (pontosUsuario === 21 && pontosMaquina === 21){
+        console.log("Deu empate!")
+      } else if (pontosUsuario <= 17){
+        while (pontosMaquina > pontosUsuario){
+          return (pontosMaquina += carta.valor)
+        }  if (pontosUsuario > 21){
+          console.log("Você perdeu!")
+        }/* else if () { */
+    
+        }if(pontosMaquina > 21){
+          console.log("Usuario Ganhou!")
+        }
+      }
+    }
+    
+     else{
+      console.log("O jogo acabou.")
+    }
